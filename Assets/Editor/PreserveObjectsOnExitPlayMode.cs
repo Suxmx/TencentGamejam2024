21e8e68aace44cbda85d01b271c31a52c5bf81b2
chengjun_X <u202112182@hust.edu.cn>
2024-10-21 17:23:44 +0800

    白盒

---
+++ Assets/Editor/PreserveObjectsOnExitPlayMode.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class PreserveChangesInPlayMode
+{
+    static PreserveChangesInPlayMode()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.EnteredEditMode)
+        {
+            // 在此处执行保存逻辑，例如保存所有修改的对象
+            foreach (var obj in Object.FindObjectsOfType<GameObject>())
+            {
+                EditorUtility.SetDirty(obj);
+            }
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
