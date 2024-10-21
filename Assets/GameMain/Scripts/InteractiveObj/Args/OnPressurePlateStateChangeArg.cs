7548898e642fffe8c956fec8f89a5bf8b3dc0db5
yanxi <424504326@qq.com>
2024-10-21 15:15:28 +0800

    feat:增加压力板和移动平台联动

---
+++ Assets/GameMain/Scripts/InteractiveObj/Args/OnPressurePlateStateChangeArg.cs
@@ -0,0 +1,20 @@
+﻿using Framework.Develop;
+
+namespace Tencent.Args
+{
+    public class OnPressurePlateStateChangeArg : GameEventArgs
+    {
+        public static int EventId = typeof(OnPressurePlateStateChangeArg).GetHashCode();
+        public override int Id => EventId;
+
+        public string TriggerKey;
+        public bool Enable;
+        public static OnPressurePlateStateChangeArg Create(string triggerKey,bool enable)
+        {
+            var e = new OnPressurePlateStateChangeArg();
+            e.TriggerKey = triggerKey;
+            e.Enable = enable;
+            return e;
+        }
+    }
+}
