e31c4db388bb1641ef6c859200457db4b02fee60
yanxi <424504326@qq.com>
2024-10-25 16:42:15 +0800

    merge

---
+++ Assets/GameMain/Scripts/InteractiveObj/SceneObj/FinalPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Framework;
+using UnityEngine;
+
+namespace Tencent
+{
+    public class FinalPoint : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<PlayerTrigger>(out var playerTrigger))
+            {
+                AGameManager.Instance.LevelWin();
+            }
+        }
+    }
+}
--- Assets/GameMain/Scripts/Procedure/ProcedureMain.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedureMain.cs
@@ -26,18 +26,20 @@ namespace GameMain
 
         public void LevelWin()
         {
-            if (_levelIndex < LEVELMAX)
-            {
-                _levelIndex++;
-                Owner.SetValue("Level", _levelIndex);
-                Owner.SetValue("PlayerSpawnPoint", 1);
-                Owner.SetValue("NextScene", $"Level{_levelIndex}");
-                ChangeState<ProcedureChangeScene>();
-            }
-            else
-            {
-                //TODO:End
-            }
+            // if (_levelIndex < LEVELMAX)
+            // {
+            //     _levelIndex++;
+            //     Owner.SetValue("Level", _levelIndex);
+            //     Owner.SetValue("PlayerSpawnPoint", 1);
+            //     Owner.SetValue("NextScene", $"Level{_levelIndex}");
+            //     ChangeState<ProcedureChangeScene>();
+            // }
+            // else
+            // {
+            //     //TODO:End
+            // }
+            Owner.SetValue("NextScene", "EndScene");
+            ChangeState<ProcedureChangeScene>();
         }
 
         public void PlayerDie()
